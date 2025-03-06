e0ac2eafece98a32fd9280ec29d6f746e5ebea2b
root <root@LUCIANO-JUNIOR>
2025-03-05 22:02:28 -0300

    Add Laravel Breeze para autenticação

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',  // Permite que o Vite seja acessado pelo Docker
+        port: 5173,       // Garante que a porta seja usada
+        strictPort: true,
+        hmr: {
+            host: 'localhost',  // Se estiver rodando na máquina
+            protocol: 'ws',
+        },
+        watch: {
+            usePolling: true, // Necessário para Docker em alguns casos
+        },
+    },
 });
